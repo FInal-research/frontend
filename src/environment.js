beb8079659b1f2a8b896e9bd677557745c30aece
Kalana <kalanaheshan47@gmail.com>
2023-04-27 22:48:23 +0530

    change:url

--- src/environment.js
+++ src/environment.js
@@ -1,1 +1,2 @@
-export const base_url = "http://localhost:8000/api/";
+// export const base_url = "http://localhost:8000/api/";
+export const base_ril = "https://research-backend47.herokuapp.com/api/";
